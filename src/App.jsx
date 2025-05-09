6f996c851c671dcd0d52092d1c7b7bc973c69525
VusanDebnath <sm.vusandebnath@gmail.com>
2025-05-09 22:36:49 +0600

    try commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Gallery from "./components/Gallery";
+
+function App() {
+  return (
+    // JSX code
+    <>
+      <div className="App">
+        <h1>My React App</h1>
+        <p>This is a simple React application.</p>
+      </div>
+      <Gallery />
+    </>
+  );
+}
+export default App;
